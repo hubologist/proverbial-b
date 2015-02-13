1f0c4df47fb0451d1f64c07d3e9e5c48dc3f647a
hubologist <hubologist@gmx.com>
2015-02-13 23:12:23 +0000

    Refactoring proverbial with jQuery. source.txt became source.json

--- js/custom.js
+++ js/custom.js
@@ -7,22 +7,58 @@
  *
  * */
 
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex ;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
 $(document).ready(function () {
     console.log("Document ready.");
 });
 
 var source = 'source.json';
 
-var jqxhr = $.getJSON(source, function (json) {
+$.getJSON(source, function (json) {
     var i = 0;
+    var seed = [];
+
+    for (i = 0; i < json.length; i++) {
+        seed[i] = i;
+    }
+
+    seed = shuffle(seed);
+
+    $("#proverb").html(json[seed[i-1]]);
 
     $(document).keydown(function (e) {
         if (e.which == 37 || e.which == 38) {
             i--;
-            console.log(json[i]);
+            if (i < 0) {
+                i = seed.length - 1;
+            }
+            $("#proverb").html(json[seed[i]]);
+            $("#proverb").removeClass().addClass("fadeInUp animated");
         } else if (e.which == 39 || e.which == 40) {
             i++;
-            console.log(json[i]);
+            if (i > seed.length - 1) {
+                i = 0;
+            }
+            $("#proverb").html(json[seed[i]]);
+            $("#proverb").removeClass().addClass("fadeInDown animated");
         }
     });
 })
